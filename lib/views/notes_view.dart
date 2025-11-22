fa0c049fca5ae81d8b0bc14e1b2df44ca706da9d
Abdulrhman755 <strikelord945@gmail.com>
2025-11-22 17:36:32 +0200

    Added custom app bar to the edit note

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -29,3 +29,5 @@ class NotesView extends StatelessWidget {
 }
 
 
+
+
--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,13 +10,17 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          const SizedBox(height: 32),
-          CustomTextField(hint: 'Title'),
-          const SizedBox(height: 16),
-          CustomTextField(hint: 'Content', maxLines: 5),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 32),
+            CustomTextField(hint: 'Title'),
+            const SizedBox(height: 16),
+            CustomTextField(hint: 'Content', maxLines: 5),
+            const SizedBox(height: 32),
+            const CustomButton(),
+          ],
+        ),
       ),
     );
   }
