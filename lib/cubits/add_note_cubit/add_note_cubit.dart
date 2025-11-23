6f6fa8d2cd7506c0bd41ca70898ce52cd8888714
Abdulrhman755 <strikelord945@gmail.com>
2025-11-23 11:45:44 +0200

    Refactor addNote method in AddNoteCubit for improved readability and structure

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
@@ -7,9 +11,15 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
 
-  addNote(NoteModel note)
-  {
-
+    try {
+      var notesBox = Hive.box(kNotesBox);
+      emit(AddNoteSuccess());
+      await notesBox.add(note);
+    } catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
   }
 }
